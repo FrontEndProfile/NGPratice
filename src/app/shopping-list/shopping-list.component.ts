323bde3b847eb662f9475ba5a1846947612660bb
Fazal-Code <fazal.rehman@techsembly.com>
2024-02-12 15:54:01 +0500

    ais project k last kuch bi smj nhi a raha kya bana raha ha

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Ingredients } from '../shared/ingredient.model'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
 export class ShoppingListComponent {
+  ingredients : Ingredients[] = [
+    new Ingredients('Apples',5),
+    new Ingredients('Mango',3),
+  ]
 
+  
 }
